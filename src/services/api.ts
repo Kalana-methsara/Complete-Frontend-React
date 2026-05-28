7016aced7c1b48f91e64db0eef3e377d1ad2e441
Kalana-methsara <kalanamethsara53@gmail.com>
2026-05-28 12:43:27 +0530

    a

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ import { STORAGE_KEYS } from "../utils/storageKeys";
 //   },
 // });
 const api = axios.create({
-  baseURL: "https://complete-backend-react.vercel.app/"
+  baseURL: "https://complete-backend-react.vercel.app"
 });
 
 const PUBLIC_ENDPOINTS = ["/auth/login", "/auth/register", "/auth/refresh"];
